aa24caec95e4aa7e6b1b597d30781a790927ba24
1rvyn <i.hall3@rgu.ac.uk>
2023-03-02 13:34:34 +0000

    structure change

--- routes/home.go
+++ routes/home.go
@@ -3,7 +3,7 @@ package routes
 import "github.com/gofiber/fiber/v2"
 
 func Home(c *fiber.Ctx) error {
-	return c.Render("home", fiber.Map{
+	return c.Render("index", fiber.Map{
 		"Title": "Home",
 	})
 }
