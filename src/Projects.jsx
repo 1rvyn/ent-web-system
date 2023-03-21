757f5b53cd16f75547af622c37364a5d405338c8
1rvyn <i.hall3@rgu.ac.uk>
2023-03-21 20:28:43 +0000

    implement daily wages (broken)

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 function Projects(props) {
   const [isFetching, setIsFetching] = useState(false);
   const [projects, setProjects] = useState([]);
+  const [payRateMode, setPayRateMode] = useState('hourly');
   const [createdProjects, setCreatedProjects] = useState([]);
   const [currentWorkerType, setCurrentWorkerType] = useState('intern');
   const [workerDetails, setWorkerDetails] = useState({
@@ -19,15 +20,18 @@ function Projects(props) {
     const workerDetailsCopy = { ...workerDetails };
     const currentDetails = workerDetailsCopy[currentWorkerType];
 
+    const hourlyWage =
+        payRateMode === 'hourly'
+            ? Number(currentDetails.hourlyWage)
+            : Number(currentDetails.hourlyWage || 0) / 8;
+
     const newWorker = {
       type: currentWorkerType,
       numWorkers: Number(currentDetails.numWorkers),
-      hourlyWage: Number(currentDetails.hourlyWage),
+      hourlyWage: hourlyWage,
       numHours: Number(currentDetails.numHours),
+      payRateMode,
     };
-
-    // allow for titles to be added to projects
-
 
     updatedProjects.push(newWorker);
 
@@ -40,13 +44,20 @@ function Projects(props) {
     });
   };
 
+
+
+  const handlePayRateToggle = () => {
+    setPayRateMode(payRateMode === 'hourly' ? 'daily' : 'hourly');
+  };
+
+
   const handleCreateProject = async (event) => {
     event.preventDefault();
     setIsFetching(true);
 
     const projectToCreate = {
-        title: workerDetails.title,
-        workers: projects
+      title: workerDetails.title,
+      workers: projects
     }
 
     console.log("New project:", JSON.stringify(projectToCreate));
@@ -102,13 +113,21 @@ function Projects(props) {
     });
   };
 
-  const handleHourlyWageChange = (event) => {
+  const handlePayRateChange = (event) => {
     const { value } = event.target;
+    let newValue;
+
+    if (payRateMode === 'hourly') {
+      newValue = value;
+    } else {
+      newValue = value / 8;
+    }
+
     setWorkerDetails({
       ...workerDetails,
       [currentWorkerType]: {
         ...workerDetails[currentWorkerType],
-        hourlyWage: value,
+        hourlyWage: newValue,
       },
     });
   };
@@ -130,90 +149,115 @@ function Projects(props) {
     const buttonText = workerType.charAt(0).toUpperCase() + workerType.slice(1);
 
     return (
-      <button
-        key={workerType}
-        className={className}
-        onClick={() => handleWorkerTypeClick(workerType)}
-      >
-        {buttonText}
-      </button>
+        <button
+            key={workerType}
+            className={className}
+            onClick={() => handleWorkerTypeClick(workerType)}
+        >
+          {buttonText}
+        </button>
     );
   };
-  
+
 
   const currentDetails = workerDetails[currentWorkerType];
 
   return (
-    <div>
-      {isLoggedIn ? (
-        <>
-          <p>You are logged in. Welcome back!</p>
-          <div className="worker-buttons">
-            {['intern', 'junior', 'mid', 'senior'].map(renderWorkerButton)}
-          </div>
-          <div className="worker-form">
-            <h3>
-              {currentWorkerType.charAt(0).toUpperCase() + currentWorkerType.slice(1)} Workers
-            </h3>
-            <label>
-              Number of Workers:
-              <input
-                type="number"
-                value={currentDetails.numWorkers}
-                onChange={handleNumWorkersChange}
-              />
-            </label>
-            <br />
-            <label>
-              Hourly Wage:
-              <input
-                type="number"
-                value={currentDetails.hourlyWage}
-                onChange={handleHourlyWageChange}
-              />
-            </label>
-            <br />
-            <label>
-              Number of Hours:
-              <input
-                type="number"
-                value={currentDetails.numHours}
-                onChange={handleNumHoursChange}
-              />
-            </label>
-            <br />
-            <button onClick={handleAddWorker}>Add {currentWorkerType} Workers</button>
-          </div>
-          <div className="project-form">
-            <h3>Current project:</h3>
-            <form onSubmit={handleCreateProject}>
-              <label>
-                Project Title:
-                <input type="text" value={workerDetails.title} onChange={handleTitleChange} />
-              </label>
-              <br />
-              <ul>
-                {projects.map((project, index) => (
-                    <li key={index}>
-                      {project.numWorkers} x {project.type && project.type.charAt(0).toUpperCase() + project.type.slice(1)} Employees
-                      <br />
-                      {project.numHours}h/week | @ ${project.hourlyWage}/h
-                      <br />
-                      Total: ${project.numWorkers * project.numHours * project.hourlyWage}/h
-                    </li>
-                ))}
-              </ul>
-              <button type="submit" disabled={isFetching}>
-                {isFetching ? 'Creating...' : 'Create a Project'}
-              </button>
-            </form>
-          </div>
+      <div>
+        {isLoggedIn ? (
+            <>
+              <p>You are logged in. Welcome back!</p>
 
-        </>
-      ) : (
-        <p>Please log in to create projects.</p>
-      )}
-    </div>
+              <div className="worker-buttons">
+                {['intern', 'junior', 'mid', 'senior'].map(renderWorkerButton)}
+              </div>
+              <div className="worker-form">
+                <h3>
+                  {currentWorkerType.charAt(0).toUpperCase() + currentWorkerType.slice(1)} Workers
+                </h3>
+                <label>
+                  Number of Workers:
+                  <input
+                      type="number"
+                      value={currentDetails.numWorkers}
+                      onChange={handleNumWorkersChange}
+                  />
+                </label>
+                <br />
+                <button onClick={handlePayRateToggle}>
+                  {payRateMode === 'hourly' ? 'Switch to Daily' : 'Switch to Hourly'}
+                </button>
+
+                <label>
+                  {payRateMode.charAt(0).toUpperCase() + payRateMode.slice(1)} Wage:
+                  <input
+                      type="number"
+                      value={
+                        payRateMode === 'hourly'
+                            ? currentDetails.hourlyWage
+                            : (currentDetails.hourlyWage || 0) * 8
+                      }
+                      onChange={handlePayRateChange}
+                  />
+
+                </label>
+                <br />
+
+                <label>
+                  Hours per Week:
+                  <input
+                      type="number"
+                      value={currentDetails.numHours}
+                      onChange={handleNumHoursChange}
+                  />
+                </label>
+                <br />
+
+                <button onClick={handleAddWorker}>Add {currentWorkerType} Workers</button>
+              </div>
+              <div className="project-form">
+                <h3>Current project:</h3>
+                <form onSubmit={handleCreateProject}>
+                  <label>
+                    Project Title:
+                    <input type="text" value={workerDetails.title} onChange={handleTitleChange} />
+                  </label>
+                  <br />
+                  <ul>
+                    {projects.map((project, index) => {
+                      const totalHourlyCost =
+                          project.payRateMode === 'hourly'
+                              ? project.numWorkers * project.numHours * project.hourlyWage
+                              : project.numWorkers * project.numHours * (project.hourlyWage * 8);
+                      const totalDailyCost =
+                          project.payRateMode === 'hourly'
+                              ? project.numWorkers * project.numHours * project.hourlyWage * 5
+                              : project.numWorkers * project.numHours * project.hourlyWage;
+
+                      return (
+                          <li key={index}>
+                            {project.numWorkers} x {project.type.charAt(0).toUpperCase() + project.type.slice(1)} Employees
+                            <br />
+                            {project.numHours}h/week | @ ${project.hourlyWage}/h | ${project.hourlyWage * 8}/day
+                            <br />
+                            Total (hourly): ${totalHourlyCost}
+                            <br />
+                            Total (daily): ${totalDailyCost}
+                          </li>
+                      );
+                    })}
+                  </ul>
+                  <button type="submit" disabled={isFetching}>
+                    {isFetching ? 'Creating...' : 'Create a Project'}
+                  </button>
+                </form>
+              </div>
+
+            </>
+        ) : (
+            <p>Please log in to create projects.</p>
+        )}
+      </div>
   );
 }
 
